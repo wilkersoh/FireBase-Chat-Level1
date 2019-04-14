4b8a5d88ca6e90d2e5470ce0d1a3966e3b7ae373
wilkersoh <plgu@hotmail.com>
2019-04-14 22:18:12 +0800

    initial

---
+++ scripts/chat.js
@@ -0,0 +1,56 @@
+// adding new chat documents
+// setting up a real0time listener to get new chats
+// updating the username
+// updating the room
+
+class Chatroom {
+    constructor(room, username){
+        this.room = room;
+        this.username = username;
+        this.chats = db.collection('chats');
+    }
+    async addChat(message){
+        // format a chat object
+        const now = new Date();
+        const chat = {
+            message,
+            username: this.username,
+            room: this.room,
+            create_at: firebase.firestore.Timestamp.fromDate(now)
+        };
+        // save the chat document 
+        const response = await this.chats.add(chat)
+        return response;
+    }
+    getChats(cb){
+        // db 里面的 room == 这个room
+        // 然后 它就只会显示 chatroom('x')的罢了 被过滤了
+        this.chats
+          .where('room', '==', this.room) 
+          .orderBy('create_at')
+        // snapshot method is firebase
+          .onSnapshot(snapshot => {
+            snapshot.docChanges().forEach(change => {
+                if(change.type === 'added'){
+                    // update to UI
+                    cb(change.doc.data());
+                }
+            })
+        })
+    }
+    updateName(username){
+        this.username = username;
+    }
+    updateRoom(room){
+        this.room = room;
+        console.log('room updated')
+    }
+}
+
+
+const chatroom = new Chatroom('general', 'shuan');
+chatroom.getChats((data) => {
+    console.log(data);
+});
+
+chatroom.updateRoom('gaming');
